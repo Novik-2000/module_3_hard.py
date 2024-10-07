d13d4ba36e5d4c495aa34fa2a9eb4c4fb83d3787
Novik-2000 <lyubasha.novikova.2000@mail.ru>
2024-10-07 23:47:10 +0300

    Update module_3_hard.py

--- module_3_hard.py
+++ module_3_hard.py
@@ -17,11 +17,11 @@ def calculate_structure_sum(data) :
 
 
 data_structure = [
-    [1, 2, 4],
-    {'a': 4, 'b': 5},
-    (6,),
-    "Hello",
-    ((), [{(2, 'Urban', ('Urban2', 35))}])
+[1, 2, 3],
+{'a': 4, 'b': 5},
+(6, {'cube': 7, 'drum': 8}),
+"Hello",
+((), [{(2, 'Urban', ('Urban2', 35))}])
 ]
 
 # 1 + 2 + 3 + len('a') + 4 + len('b') + 5 + 6 + len('cube') + 7 + .... + 35 = 99
